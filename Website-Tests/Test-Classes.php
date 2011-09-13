b17b88038635253c6337090852a47c63910d20de
Steve-Fenton <info@enhance-php.com>
2011-09-13 19:56:28 +0100

    Latest zip archive and examples. Signed-off-by: Steve-Fenton <info@enhance-php.com>

--- Website-Tests/Test-Classes.php
+++ Website-Tests/Test-Classes.php
@@ -18,13 +18,13 @@ class ExampleClassTests extends EnhanceTestFixture
                 $this->target = Enhance::getCodeCoverageWrapper('ExampleClass');
         }
 
-        public function addTwoNumbersWith3and2Expect5Test()
+        public function addTwoNumbersWith3and2Expect5()
         {
                 $result = $this->target->addTwoNumbers(3, 2);
                 Assert::areIdentical(5, $result);
         }
         
-        public function addTwoNumbersWith4and2Expect6Test()
+        public function addTwoNumbersWith4and2Expect6()
         {
                 $result = $this->target->addTwoNumbers(4, 2);
                 Assert::areIdentical(6, $result);
