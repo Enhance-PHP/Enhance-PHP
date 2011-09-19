103f595ec2808aec6bdcdd1e7073d8a6ba6ce186
Steve Fenton <info@enhance-php.com>
2011-09-19 21:08:38 +0100

    Corrected spelling on "NOT" text resource. Signed-off-by: Steve Fenton <info@enhance-php.com>

--- EnhanceTestFramework.php
+++ EnhanceTestFramework.php
@@ -214,7 +214,7 @@ class TextEn
 {
     public $FormatForTestRunTook = 'Test run took {0} seconds';
     public $FormatForExpectedButWas = 'Expected {0} but was {1}';
-    public $FormatForExpectedButWas = 'Expected NOT {0} but was {1}';
+    public $FormatForExpectedNotButWas = 'Expected NOT {0} but was {1}';
     public $FormatForExpectedContainsButWas = 'Expected to contain {0} but was {1}';
     public $FormatForExpectedNotContainsButWas = 'Expected NOT to contain {0} but was {1}';
 
