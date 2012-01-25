11f214bdb8882b9190ee6d0c673be063a04e3380
Steve Fenton <info@enhance-php.com>
2012-01-25 23:14:01 +0000

    Missing leading \ on Exception. Signed-off-by: Steve Fenton <info@enhance-php.com>

--- EnhanceTestFramework.php
+++ EnhanceTestFramework.php
@@ -1220,7 +1220,7 @@ class Assertions
 
 class TestException extends \Exception
 {
-    public function __construct($message = null, $code = 0, Exception $previous = null)
+    public function __construct($message = null, $code = 0, \Exception $previous = null)
     {
         parent::__construct($message, $code, $previous);
 
