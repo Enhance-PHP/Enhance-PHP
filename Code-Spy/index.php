5459f14dc2defcbf8199660e298b50c3687d7f31
Steve Fenton <info@enhance-php.com>
2012-03-03 12:07:00 +0000

    Update to latest version of Codespy in Codespy example. Signed-off-by: Steve Fenton <info@enhance-php.com>

--- Code-Spy/index.php
+++ Code-Spy/index.php
@@ -7,6 +7,7 @@ include('codespy.php');
 \codespy\Analyzer::$outputdir = 'C:\Users\Steve Fenton\Documents\_BackedUp\Projects\enhance-php-source\Code-Spy\output';
 \codespy\Analyzer::$outputformat = 'html';
 \codespy\Analyzer::$coveredcolor = '#c2ffc2';
+\codespy\Analyzer::addFunctionToAnalyze('ExampleClass.php', 'addTwoNumbers', 'ExampleClass');
 
 // Find the tests - '.' is the current folder
  \Enhance\Core::discoverTests('.');
