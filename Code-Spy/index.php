8b07cab24a2d3a43aaca4d5498cf1a1aa428cc4f
sandeepcr2 <sandeepcr2@gmail.com>
2012-03-17 23:45:05 +0530

    Codespy Updated

--- Code-Spy/index.php
+++ Code-Spy/index.php
@@ -1,10 +1,10 @@
 <?php
 // Set up the code-coverage reporting
 include('codespy.php');
-\codespy\Analyzer::$outputdir = 'C:\Users\Steve Fenton\Documents\_BackedUp\Projects\enhance-php-source\Code-Spy\Output';
+\codespy\Analyzer::$outputdir = 'g:\work\EasyPHP-5.3.6.1\www\output';
 \codespy\Analyzer::$outputformat = 'html';
 \codespy\Analyzer::$coveredcolor = '#c2ffc2';
-\codespy\Analyzer::addFileToSpy('ExampleClass.php');
+//\codespy\Analyzer::addFileToSpy('ExampleClass.php');
 
 // Include the test framework
 include('../EnhanceTestFramework.php');
