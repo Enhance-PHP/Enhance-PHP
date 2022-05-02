8984c70c039d3b3eddfa275f33651f35cce850fb
Steve Fenton <github@stevefenton.co.uk>
2022-05-02 20:23:30 +0100

    Updating examples to use include_once

--- Self-Test/index.php
+++ Self-Test/index.php
@@ -1,12 +1,12 @@
 <?php
 // Set up the code-coverage reporting
-//include('../Code-Spy/codespy.php');
+//include_once('../Code-Spy/codespy.php');
 //\codespy\Analyzer::$outputdir = 'C:\Users\Steve Fenton\Documents\_BackedUp\Projects\enhance-php-source\Code-Spy\output';
 //\codespy\Analyzer::$outputformat = 'html';
 //\codespy\Analyzer::$coveredcolor = '#c2ffc2';
 
 // Include the test framework
-include('../EnhanceTestFramework.php');
+include_once('../EnhanceTestFramework.php');
 
 // Find the tests - '.' is the current folder
 \Enhance\Core::discoverTests('.', true, array('Exclusion'));
